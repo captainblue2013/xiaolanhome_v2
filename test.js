872402b153da424be6fbff7e29c91421d2d8ff3f
captainblue2013 <admin@lanhao.name>
2016-06-12 15:42:58 +0800

    about me

--- test.js
+++ test.js
@@ -1,3 +1,14 @@
 /**
  * Created by lanhao on 16/6/11.
  */
+const requestAgent = require('request-agent').init();
+
+requestAgent.url('http://127.0.0.1:3001/loadMore')
+.headers({'content-type':'application/json'})
+.body({'from':0})
+.method('get')
+.send()
+.then(requestAgent.toJson)
+.then((value) => {
+    console.log(value);
+});
