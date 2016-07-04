d6345e9462119f4c7101a720e5e3911f69a2ab64
captainblue2013 <admin@lanhao.name>
2016-07-04 13:44:15 +0800

    format

--- controllers/index.js
+++ controllers/index.js
@@ -8,61 +8,64 @@ var Controller = {};
 
 Controller.tagSet = null;
 
-Controller.index = (req,res) => {
-    articleModel.list()
-        .then((list)=>{
-            res.render('index.html',{'list':list});
-        })
-        .catch((err)=>{
-            console.log(err);
-            res.render('error.html',{});
-        });
+Controller.index = (req, res) => {
+  articleModel.list()
+    .then((list)=> {
+      res.render('index.html', {'list': list});
+    })
+    .catch((err)=> {
+      console.log(err);
+      res.render('error.html', {});
+    });
 };
 
-Controller.item = (req,res) =>{
-    let id = req.params[1];
-    articleModel.detail(id)
-        .then( (item) => {
-            item?
-                res.render('detail.html',{'item':item})
-                :res.render('error.html',{});
-        })
-        .catch( (err) => {
-            console.log(err);
-            res.render('error.html',{});
-        });
+Controller.item = (req, res) => {
+  let id = req.params[1];
+  if(req.params[0]!=='article'){
+    id = req.params[0];
+  }
+  articleModel.detail(id)
+    .then((item) => {
+      item ?
+        res.render('detail.html', {'item': item})
+        : res.render('error.html', {});
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render('error.html', {});
+    });
 };
 
 Controller.about = (req, res) => {
-    res.render('about.html',{});
+  res.render('about.html', {});
 };
 
 Controller.search = (req, res) => {
-    res.render('search.html',{});
+  res.render('search.html', {});
 };
 
 Controller.tags = (req, res) => {
-    if(!Controller.tagSet){
-        loadTagSet();
-    }
+  if (!Controller.tagSet) {
+    loadTagSet();
+  }
 
-    res.render('tags.html',{tags:Array.from(Controller.tagSet)});
+  res.render('tags.html', {tags: Array.from(Controller.tagSet)});
 };
 
-Controller.building = (req,res) => {
-    res.render('building.html',{});
+Controller.building = (req, res) => {
+  res.render('building.html', {});
 };
 
-const loadTagSet = () =>{
-    articleModel.list({size:999})
-        .then((list)=>{
-            Controller.tagSet = new Set();
-            for(let k in list){
-                list[k]._source.keywords.forEach((item)=>{
-                    Controller.tagSet.add(item);
-                });
-            }
+const loadTagSet = () => {
+  articleModel.list({size: 999})
+    .then((list)=> {
+      Controller.tagSet = new Set();
+      for (let k in list) {
+        list[k]._source.keywords.forEach((item)=> {
+          Controller.tagSet.add(item);
         });
+      }
+    });
 };
 
 module.exports = Controller;
--- routes.js
+++ routes.js
@@ -1,12 +1,13 @@
 'use strict';
 
 module.exports = {
-    '/':'get.index.index',
-    '/article/search':'get.article.search',
-    '/article/{id}':'get.index.item',
-    '/loadMore':'get.article.loadMore',
-    '/tags':'get.index.tags',
-    '/about':'get.index.about',
-    '/project':'get.index.building',
-    '/search':'get.index.search'
+  '/': 'get.index.index',
+  '/article/search': 'get.article.search',
+  '/article/{id}': 'get.index.item',
+  '/loadMore': 'get.article.loadMore',
+  '/tags': 'get.index.tags',
+  '/about': 'get.index.about',
+  '/project': 'get.index.building',
+  '/search': 'get.index.search',
+  '/{id}': 'get.index.item',
 };
