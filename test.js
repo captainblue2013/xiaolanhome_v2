2c76845a670943729d2361f8ca050437cf5a1d9a
captainblue2013 <admin@lanhao.name>
2017-01-23 19:53:16 +0800

    no message

--- test.js
+++ test.js
@@ -1,12 +1,23 @@
 "use strict";
-let a = function () {
+const request = require('request-agent').init();
 
+const vote = function () {
+  request.url('http://www.ctcnn.com/jinlvAward2016/vote.jsp?id=270&awardIterm_id=15')
+    .body({
+      awardIterm_id:15,
+      id:270
+    })
+    .method('post')
+    .send()
+    .then(function (res) {
+      if(res.statusCode == 200){
+        console.log(res.body);
+      }
+      setTimeout(function () {
+        vote();
+      },1000);
+    });
 };
 
-let b = ()=>{};
 
-console.log(a,b);
-//[Function] [Function]
-
-console.log(a.prototype,b.prototype);
-//{} undefined
+vote();
