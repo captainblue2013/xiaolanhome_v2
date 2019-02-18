2b3a6c8d8d23937b84faa30fda87f6dfefb9bb7e
lanhao <lanhao@tik.com>
2019-02-18 22:28:37 +0800

    318

--- run.js
+++ run.js
@@ -1,3 +1,28 @@
-/**
- * Created by lanhao on 16/6/2.
- */
+const fs = require('fs');
+const Md = require('markdown').markdown;
+
+const file = process.argv[2];
+
+if (!fs.existsSync(file)) {
+  console.log(`${file} Not Found!`);
+  process.exit();
+}
+
+const content = fs.readFileSync(file).toString();
+
+const html = Md.toHTML(content);
+
+const id = 318;
+const t = Date.now();
+fs.writeFileSync(`./articles/${id}.json`, JSON.stringify({
+  id,
+  title: '2018总结: 你咋才来啊',
+  keywords: [
+    'docker',
+    'devops',
+    'node'
+  ],
+  content: html,
+  createdAt: Number.parseInt(t / 1000),
+  modified: Number.parseInt(t / 1000),
+}, null, 2));
